c7a6e7b5e106dc26db482c305b775557b7f11188
Meijuta <meijuta@proton.me>
2025-02-21 21:19:51 +1100

    starting a framework..?

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
-hello
+#include <iostream>
+#include <array>
+#include <vector>
+
+std::array<unsigned int, 2> windowDimensions;
+std::array<unsigned int, 2> worldDimensions;
+
+unsigned int RNG(unsigned int max)
+{
+    // TODO
+}
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
