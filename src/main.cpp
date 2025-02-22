0b05df740d976d3c489c2aa86d3f1863d3da3813
Meijuta <meijuta@proton.me>
2025-02-22 15:41:14 +1100

    Prepared Square Function

--- src/main.cpp
+++ src/main.cpp
@@ -86,6 +86,15 @@ void printMatrixInTerminal(ByteMatrix matrix)
   }
 }
 
+
+
+
+// Shape Drawing Functions //
+void drawSquare(std::array<unsigned int, 2> coord1, std::array<unsigned int, 2> coord2, std::array<unsigned char, 3> RGB)
+{
+  // TODO
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
 }
