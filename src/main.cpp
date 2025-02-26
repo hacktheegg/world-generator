950c2bff2a4cf55689d7d7db3dd13d99b33ba42b
hacktheegg <>
2025-02-26 18:44:22 +1100

    multiple triangles arranged in diagonally alligned squares

--- src/main.cpp
+++ src/main.cpp
@@ -183,14 +183,15 @@ int main()
 
   // vertices and vertice attributes
   float vertices[] = {
-     0.5f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
+    -0.75f,  0.75f, 0.0f,  0.00f,  0.75f, 0.0f,  0.75f,  0.75f, 0.0f,
+    -0.75f,  0.00f, 0.0f,  0.00f,  0.00f, 0.0f,  0.75f,  0.00f, 0.0f,
+    -0.75f, -0.75f, 0.0f,  0.00f, -0.75f, 0.0f,  0.75f, -0.75f, 0.0f
   };
   unsigned int indices[] = {
     0, 1, 3,
-    1, 2, 3
+    1, 3, 4,
+    4, 5, 7,
+    5, 7, 8
   };  
   unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
@@ -241,7 +242,7 @@ int main()
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
     //glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
     // glBindVertexArray(0); // no need to unbind it every time
 
 
