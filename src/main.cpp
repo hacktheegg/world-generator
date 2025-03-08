a0ff023de065dcf89194ee915ade5b70241a4544
hacktheegg <>
2025-03-08 14:29:33 +1100

    2 squares of different colours (Amazing)

--- src/main.cpp
+++ src/main.cpp
@@ -193,26 +193,11 @@ void processInput(GLFWwindow *window) {
 
 
 
-const char *vertexShaderSource = "#version 330 core\n"
-  "layout (location = 0) in vec3 aPos;\n"
-  "void main()\n"
-  "{\n"
-  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-  "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-  "out vec4 FragColor;\n"
-  "void main()\n"
-  "{\n"
-  "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-  "}\n\0";
-
-
-
-
 int main()
 {
   std::cout << "Hello, World!" << std::endl;
 
+  /*
   const ByteMatrix worldNoise = generateNoise(63,127,0.08, 5);
   char c;
   while (true)
@@ -243,6 +228,7 @@ int main()
       break;
     }
   }
+  */
 
 
 
@@ -276,15 +262,21 @@ int main()
 
   // vertices and vertice attributes
   float vertices[] = {
-    -0.75f,  0.75f, 0.0f,  0.00f,  0.75f, 0.0f,  0.75f,  0.75f, 0.0f,
-    -0.75f,  0.00f, 0.0f,  0.00f,  0.00f, 0.0f,  0.75f,  0.00f, 0.0f,
-    -0.75f, -0.75f, 0.0f,  0.00f, -0.75f, 0.0f,  0.75f, -0.75f, 0.0f
+    -0.75f, -0.75f, 0.0f, 1.00f, 0.50f, 0.00f,
+     0.00f, -0.75f, 0.0f, 1.00f, 0.50f, 0.00f,
+    -0.75f,  0.00f, 0.0f, 1.00f, 0.50f, 0.00f,
+     0.00f,  0.00f, 0.0f, 1.00f, 0.50f, 0.00f,
+
+     0.75f,  0.75f, 0.0f, 0.50f, 1.00f, 0.00f,
+     0.00f,  0.75f, 0.0f, 0.50f, 1.00f, 0.00f,
+     0.75f,  0.00f, 0.0f, 0.50f, 1.00f, 0.00f,
+     0.00f,  0.00f, 0.0f, 0.50f, 1.00f, 0.00f
   };
   unsigned int indices[] = {
-    0, 1, 3,
-    1, 3, 4,
-    4, 5, 7,
-    5, 7, 8
+    0, 1, 2,
+    1, 2, 3,
+    4, 5, 6,
+    5, 6, 7
   };  
   unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
@@ -299,8 +291,11 @@ int main()
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
   //glBindBuffer(GL_ARRAY_BUFFER, 0);
